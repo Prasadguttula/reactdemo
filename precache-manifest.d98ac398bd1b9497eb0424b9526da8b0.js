59b6b08971cf620abd4ded2bf539f9dc0ebfc545
Prasadguttula <prasadguttula68@gmail.com>
2020-01-26 18:05:03 +0530

    Updates

---
+++ precache-manifest.d98ac398bd1b9497eb0424b9526da8b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51dfedcaa777394501081022e23eda25",
+    "url": "/reactdemo/index.html"
+  },
+  {
+    "revision": "d204a5ab2d1c452cceae",
+    "url": "/reactdemo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "95b661d4d4129b38619a",
+    "url": "/reactdemo/static/js/2.6949f162.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactdemo/static/js/2.6949f162.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d204a5ab2d1c452cceae",
+    "url": "/reactdemo/static/js/main.63a8c9ff.chunk.js"
+  },
+  {
+    "revision": "2ec2cf45f487086e90d5",
+    "url": "/reactdemo/static/js/runtime-main.ae84b3b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactdemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
